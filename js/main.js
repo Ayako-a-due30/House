aa228e290dfb48ae14ef47b80b26843e10dd311f
Ayako-a-due30 <sonnige.seite.str@gmail.com>
2023-07-09 07:13:20 +0900

    トップボタンで詰まっています。

--- js/main.js
+++ js/main.js
@@ -1,13 +1,16 @@
 // 'use strict'; 
-// {
-//     // 建築事例を動画と重ねる
-//     const showProject = (entries) => {
-//         const keyframes = {
-//             translate:['200px 0',0],
-//         }
-//         entries[0].target.animate(keyframes,600);
-//         console.log(entries[0].target);
-//     };
-//     const projectObserver = new IntersectionObserver(showProject);
-//     projectObserver.observe(document.querySelector('#project'));
-// }
+{
+//トップに戻るボタン
+const toTopButton = document.getElementById('top-button');
+
+window.addEventListener('scroll',()=>{
+    const scrolled = window.scrollY;
+    console.log(scrolled);
+    if (scrolled > 700) {
+        toTopButton.classList.add('show-button');
+    }else {
+        toTopButton.classList.remove('show-button');
+    }
+});
+}
+
