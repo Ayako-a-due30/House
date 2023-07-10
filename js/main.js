131b6c482ef88b9a3b3e3ff6e889c25bbead9fee
Ayako-a-due30 <sonnige.seite.str@gmail.com>
2023-07-10 13:38:31 +0900

    レスポンシブサイトの重なりが完成していない。

--- js/main.js
+++ js/main.js
@@ -1,16 +1,30 @@
 // 'use strict'; 
 {
-//トップに戻るボタン
+//トップに戻るボタンーーーーーーーーーーーーーーーーーーー
 const toTopButton = document.getElementById('top-button');
 
 window.addEventListener('scroll',()=>{
     const scrolled = window.scrollY;
-    console.log(scrolled);
     if (scrolled > 700) {
         toTopButton.classList.add('show-button');
     }else {
         toTopButton.classList.remove('show-button');
     }
 });
+//レスポンシブのトグルメニューーーーーーーーーーーーーーーーーーー
+
+const menuButton = document.getElementById('menu-button');
+const navi = document.getElementById('navi');
+const mask = document.getElementById('mask');
+
+menuButton.addEventListener('click',()=>{
+    navi.classList.toggle('show-button');
+    menuButton.classList.toggle('active');
+    mask.classList.toggle('open');
+});
+
+
+
+
 }
 
